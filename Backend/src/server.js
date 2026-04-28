4d0fc43a351683207971ec17f07e398e342dc11d
Gurveer88 <gurveerpartapsingh7595@gmail.com>
2026-04-29 00:39:39 +0530

    New limit added to json middleware

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({limit: "10mb"}));
 app.use(cookieparser());
 
 const __dirname = path.resolve();
