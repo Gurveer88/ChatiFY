49457c5067aec11ffe3b580fc164b5bffbf44607
Gurveer88 <gurveerpartapsingh7595@gmail.com>
2026-04-29 18:40:18 +0530

    Rate limiting setted up

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -12,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json({limit: "10mb"}));
+app.use(express.urlencoded({extended: true, limit: "10mb"}));
 app.use(cookieparser());
 
 const __dirname = path.resolve();
